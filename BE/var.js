4270a2085295d0696dbc61a43b626dc604d4c359
Muhammad Nasrulloh <muhammadnasrulloh.id@gmail.com>
2024-07-24 17:02:42 +0700

    up

--- BE/var.js
+++ BE/var.js
@@ -2,6 +2,6 @@ const node_production = true;
 const socketUrl = node_production ? 'https://socket.itemstore.id': 'http://localhost:4000'
 const apiUrl = node_production ? 'https://api.itemstore.id': 'http://localhost:5000'
 const feUrl = node_production ? 'https://test.itemstore.id': 'http://localhost:3000'
-const rmUrl = node_production ? 'amqp://localhost': 'amqp://localhost'
+const rmUrl = node_production ? 'amqp://34.34.222.33': 'amqp://localhost'
 
 module.exports = { socketUrl, apiUrl, feUrl, rmUrl }
