82fe7f742f95db9c023cbe5a1282eb4c7fbb78b8
Muhammad Nasrulloh <muhammadnasrulloh.id@gmail.com>
2024-07-25 08:46:24 +0700

    i[

--- BE/product.js
+++ BE/product.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 const io = require("socket.io")(4000, {
     cors: {
-        origin: ['https://test.itemstore.id']
+        origin: ['https://test.itemstore.id', 'http://localhost:3000']
     }
 })
 
